bb54bed4abd5e4f52425f6888349052f87336c76
kira <casabarra228@gmail.com>
2022-12-09 09:33:31 -0500

    Quotes API consumed Remotely/Locally depending if there is connection or not

--- dailyQuoteK.sh
+++ dailyQuoteK.sh
@@ -1,10 +1,24 @@
 # Quotes from influential Figures. Timeless Wisdom shown daily
 
-# VARIABLES
-api="https://type.fit/api/quotes"
-index=$(shuf -i 0-1643 -n 1)
-quote=$(curl -s $api | jq ".[$index].text")
-author=$(curl -s $api | jq -r ".[$index].author")
+# GENERAL VARIABLES
+httpCode=$(curl -s -o /null -I -w "%{http_code}" https://type.fit/api/quotes) #Get http request code only
+index=$(shuf -i 0-1643 -n 1) #Generate random index
 
-#Displaying Quote with its Author...
-echo -e "$quote \n -$author" | lolcat
+#REMOTE VARIABLES
+api="https://type.fit/api/quotes"
+quoteRemote=$(curl -s $api | jq ".[$index].text") #Picking random quote
+authorRemote=$(curl -s $api | jq -r ".[$index].author") #Picking random author
+
+#LOCAL VARIABLES
+json="/home/kira/bin/newDay/quotes.json"
+quoteLocal=$(jq ".[$index].text" $json)
+authorLocal=$(jq -r ".[$index].author" $json)
+
+
+#DISPLAYING QUOTE OF THE DAY WITH ITS AUTHOR:
+if [ $httpCode -eq 200 ]
+then
+	echo -e "$quoteRemote \n -$authorRemote" | lolcat
+else 
+	echo -e "$quoteLocal \n -$authorLocal" | lolcat
+fi
